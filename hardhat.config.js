40579c2c6f02ae372de181fa59459bf6f0ce57ca
Kashifnawaz03 <72625629+Kashifnawaz03@users.noreply.github.com>
2021-10-12 17:05:14 +0500

    Example 0 done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+//require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -9,7 +10,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     console.log(account.address);
   }
 });
-
+//const PRIVATE_KEY=process.env[Test_Private_Key];
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -18,4 +19,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  //networks: {
+   // ropsten: {
+   // url: "https://ropsten.infura.io/v3/fa46fd160fd847c39ce43273c683de53",
+   // accounts: [`0x${PRIVATE_KEY}`]
+   // }
+  //  }
+   
 };
